99a0566fb509791cb2c5350f59a14a51d3350f4d
G.Pedrozox22 <gabriellpedrozo177@gmail.com>
2026-04-07 13:42:12 -0300

    Update desafio6.cpp

--- desafio6.cpp
+++ desafio6.cpp
@@ -2,17 +2,25 @@
 
 using namespace std;
 
+
+//terminar codigo 
 int main(){
     float gastofamilia[5], media; 
     
     for (int i = 0; i < 5; i++){
-        cout << "Digite os gastos com alimentacao das familias " << i+1 << endl;
+        cout << "Digite os gastos com alimentacao da familia " << i+1 << endl;
         cin >> gastofamilia[i];
         media += gastofamilia[i]; 
     }
     
-    media= media/5;
+    media/= 5;
     cout << "Media de gastos: "<< media << endl;
+
+    for(int i=0; i < 5; i++){
+        if(gastofamilia[i] < media){
+            cout << "A familia "<< i + 1 << " tem gastos abaixo da media "
+        }
+    }
 
     
     return 0;
